64922cf768472270e9163aa2048b3261f7ec6b1e
Bufkin <sebastian.schemer@gmail.com>
2019-09-21 17:17:49 +0200

    Initial commit

--- src/com/bufkin/rain/Game.java
+++ src/com/bufkin/rain/Game.java
@@ -1,6 +1,6 @@
 package com.bufkin.rain;
 
-public class Game {
+public class Game implements Runnable {
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
@@ -9,7 +9,21 @@ public class Game {
 	private Thread thread;
 
 	public synchronized void start() {
-		this.thread = new Thread();
+		this.thread = new Thread(this, "Display");
+		this.thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			this.thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+
 	}
 
 }
