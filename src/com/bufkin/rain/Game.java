a766f1d1535a6e2d1138110644175fee06a49646
Bufkin <sebastian.schemer@gmail.com>
2019-09-24 13:58:21 +0200

    Episode 16

--- src/com/bufkin/rain/Game.java
+++ src/com/bufkin/rain/Game.java
@@ -1,116 +1,119 @@
 package com.bufkin.rain;
 
-import com.bufkin.rain.graphics.Screen;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
+import javax.swing.JFrame;
+
+import com.bufkin.rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static int width = 300;
-    public static int height = width / 16 * 9;
-    public static int scale = 3;
-    public static String title = "Rain";
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	public static String title = "Rain";
 
-    private Thread thread;
-    private JFrame frame;
-    private boolean running = false;
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
 
-    private Screen screen;
+	private Screen screen;
 
-    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    private int[] pixels = ((DataBufferInt) this.image.getRaster().getDataBuffer()).getData();
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) this.image.getRaster().getDataBuffer()).getData();
 
-    public Game() {
-        Dimension size = new Dimension(width * scale, height * scale);
-        this.setPreferredSize(size);
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		this.setPreferredSize(size);
 
-        this.screen = new Screen(width, height);
+		this.screen = new Screen(width, height);
 
-        this.frame = new JFrame();
-    }
+		this.frame = new JFrame();
+	}
 
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.frame.setResizable(false);
-        game.frame.setTitle(Game.title);
-        game.frame.add(game);
-        game.frame.pack();
-        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.frame.setLocationRelativeTo(null);
-        game.frame.setVisible(true);
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle(Game.title);
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
 
-        game.start();
-    }
+		game.start();
+	}
 
-    public synchronized void start() {
-        this.running = true;
-        this.thread = new Thread(this, "Display");
-        this.thread.start();
-    }
+	public synchronized void start() {
+		this.running = true;
+		this.thread = new Thread(this, "Display");
+		this.thread.start();
+	}
 
-    public synchronized void stop() {
-        this.running = false;
-        try {
-            this.thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	public synchronized void stop() {
+		this.running = false;
+		try {
+			this.thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void run() {
-        long lastTime = System.nanoTime();
-        long timer = System.currentTimeMillis();
-        final double ns = 1000000000.0 / 60.0;
-        double delta = 0;
-        int frames = 0;
-        int updates = 0;
-        while (this.running) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / ns;
-            lastTime = now;
-            while (delta >= 1) {
-                this.update();
-                updates++;
-                delta--;
-            }
-            this.render();
-            frames++;
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (this.running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				this.update();
+				updates++;
+				delta--;
+			}
+			this.render();
+			frames++;
 
-            if (System.currentTimeMillis() - timer > 1000) {
-                timer += 1000;
-                this.frame.setTitle(title + "   |   " + updates + " ups, " + frames + " fps");
-                updates = 0;
-                frames = 0;
-            }
-        }
-        this.stop();
-    }
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				this.frame.setTitle(title + "   |   " + updates + " ups, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
+		}
+		this.stop();
+	}
 
-    public void update() {
+	public void update() {
 
-    }
+	}
 
-    public void render() {
-        BufferStrategy bs = this.getBufferStrategy();
-        if (bs == null) {
-            this.createBufferStrategy(3);
-            return;
-        }
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
 
-        this.screen.clear();
-        this.screen.render();
+		this.screen.clear();
+		this.screen.render();
 
-        System.arraycopy(this.screen.pixels, 0, this.pixels, 0, this.pixels.length);
+		System.arraycopy(this.screen.pixels, 0, this.pixels, 0, this.pixels.length);
 
-        Graphics g = bs.getDrawGraphics();
-        g.drawImage(this.image, 0, 0, this.getWidth(), this.getHeight(), null);
-        g.dispose();
-        bs.show();
-    }
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(this.image, 0, 0, this.getWidth(), this.getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
 }
