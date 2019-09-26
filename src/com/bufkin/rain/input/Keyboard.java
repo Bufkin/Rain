84d86e4f020e0c9e32cf9a5ee706b9a377214017
Bufkin <sebastian.schemer@gmail.com>
2019-09-26 10:08:55 +0200

    Episode 18
    Added key listeners

---
+++ src/com/bufkin/rain/input/Keyboard.java
@@ -0,0 +1,34 @@
+package com.bufkin.rain.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+
+    private boolean[] keys = new boolean[120];
+    public boolean up, down, left, right;
+
+    public void update() {
+        this.up = this.keys[KeyEvent.VK_UP] || this.keys[KeyEvent.VK_W];
+        this.down = this.keys[KeyEvent.VK_DOWN] || this.keys[KeyEvent.VK_S];
+        this.left = this.keys[KeyEvent.VK_LEFT] || this.keys[KeyEvent.VK_A];
+        this.right = this.keys[KeyEvent.VK_RIGHT] || this.keys[KeyEvent.VK_D];
+
+        System.out.println(this.up);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        this.keys[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        this.keys[e.getKeyCode()] = false;
+    }
+}
